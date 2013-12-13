617bf6c51f9c204ad23575842d50e089b0ba7417
0ett <oett.95@gmail.com>
2013-12-13 12:45:35 +0100

    Installed on Android with rectangle error

---
+++ test/mobile/gen/R.java.d
@@ -0,0 +1,12 @@
+C:\GitHub\pluppolina\test\mobile\gen\com\mycompany\mygame\R.java \
+ : C:\GitHub\pluppolina\test\mobile\res\drawable-hdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\res\drawable-ldpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\res\drawable-mdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\res\drawable-xhdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\res\layout\main.xml \
+C:\GitHub\pluppolina\test\mobile\res\values\strings.xml \
+C:\GitHub\pluppolina\test\mobile\bin\res\drawable-hdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\bin\res\drawable-ldpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\bin\res\drawable-mdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\bin\res\drawable-xhdpi\ic_launcher.png \
+C:\GitHub\pluppolina\test\mobile\bin\AndroidManifest.xml \
